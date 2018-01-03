67304f06556d4e082a24a389b447676981beb6e0
Pavel <konarik135@gmail.com>
2018-01-03 17:23:51 +0100

    Almost done players, just need to make REST controllers

--- src/main/java/com/group9/config/players/PlayerController.java
+++ src/main/java/com/group9/config/players/PlayerController.java
@@ -6,8 +6,15 @@
 package com.group9.config.players;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
  
 import javax.validation.Valid;
  
@@ -120,5 +127,44 @@ public class PlayerController {
         service.deletePlayerById(id);
         return "redirect:/players";
     }
- 
+    
+    @RequestMapping(value = { "/new/random/{number}" }, method = RequestMethod.GET)
+       public String generatePlayer(@PathVariable int number, ModelMap model) {
+           for (int i = 0; i < number; i++) {
+                try {
+                     URL url12 = new URL("https://raw.githubusercontent.com/fivethirtyeight/data/master/most-common-name/adjusted-name-combinations-list.csv");
+                     URLConnection urlConn = url12.openConnection();
+                     InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream());
+                     BufferedReader buff = new BufferedReader(inStream);
+                     buff.readLine();
+                     String content2 = buff.readLine();
+                     ArrayList<String> forenames = new ArrayList<>();
+                     ArrayList<String> surnames = new ArrayList<>();
+                     while (content2 != null) {
+                         String[] splitted = content2.replaceAll("\"", "").split(",");
+                         forenames.add(splitted[1]);
+                         surnames.add(splitted[2]);
+                         content2 = buff.readLine();
+                     }
+
+
+                     Random r = new Random();
+                     Player player = new Player();
+                     player.setAge(r.nextInt(60-15) + 15);
+                     player.setSalary(r.nextInt(1000000-1) + 1);
+                     player.setDefense(r.nextInt(99-1) + 1);
+                     player.setOffense(r.nextInt(99-1) + 1);
+                     player.setForename(forenames.get((new Random()).nextInt(forenames.size())));
+                     player.setSurname(surnames.get((new Random()).nextInt(surnames.size())));
+
+                     service.savePlayer(player);
+
+                 } catch (IOException e) {
+                     System.out.print("Failed to load names from csv");
+                 }
+            }
+           
+           //model.addAttribute("success", "Employee " + player.getForename()+ " registered successfully");
+           return "success";
+       }
 }
